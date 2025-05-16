38870721679100e84c3215d2705c00c0b54504b2
keli <keligeorge558@gmail.com>
2025-05-16 18:59:41 +0300

    finish

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        matches = []
+        for candidate in word_list:
+            lower_candidate = candidate.lower()
+            if lower_candidate != self.word and sorted(lower_candidate) == sorted(self.word):
+                matches.append(candidate)
+        return matches
